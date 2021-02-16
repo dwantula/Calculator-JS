16e4961203b48f4d5ced5ecc441926ba752fbbf6
Aleksandra Kamińska <dwantula@gmail.com>
2021-02-16 15:24:05 +0100

    layout app

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
 var buttons = document.querySelectorAll('button:not(#equal-button)')
+var operators = document.querySelectorAll('operator');
 var equalButton = document.getElementById('equal-button');
 var display = document.querySelector('.display');
 var clearButton = document.querySelector('#clear-button');
+var displayValue = document.querySelector('.display').value;
 
 for (i = 0; i < buttons.length; i++) {
   buttons[i].addEventListener('click', function(event) {
@@ -11,11 +13,13 @@ for (i = 0; i < buttons.length; i++) {
 
 equalButton.addEventListener('click', function() {
   var result = eval(display.innerText);
-  display.innerText = result
+  display.innerText = result 
+
 })
 
 clearButton.addEventListener('click', function() {
-    var clear = "" ;
-    display.innerText = clear;
-    
+  var clear = "" ;
+  display.innerText = clear;
 })
+
+
